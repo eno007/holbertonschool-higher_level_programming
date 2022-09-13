16be60ced6c1cf906afc198bf34d6dea72be7b25
eno007 <eno.gupe@gmail.com>
2022-09-13 05:39:12 -0700

    Change in task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key] * 2))
+    multiplied = {}
+    for key in a_dictionary:
+        multiplied = a_dictionary[key] * 2
+    return (multiplied)
