43c97f2d51b6a9c162a2c3db22bd73343d1d6b6c
eno007 <eno.gupe@gmail.com>
2022-09-13 06:12:49 -0700

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max_score = 0
+    max_key = ""
     if a_dictionary:
-        return(max(a_dictionary.keys()))
+        for keys in a_dictionary.keys():
+            if max_score < a_dictionary[keys]:
+                max_score = a_dictionary[keys]
+                max_key = keys
+        return (max_key)
+    return None
