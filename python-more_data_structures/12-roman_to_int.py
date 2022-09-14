b08742c3b2d68ef633f9bed5f74ecde45d4e2d4b
eno007 <eno.gupe@gmail.com>
2022-09-14 01:29:18 -0700

    Update not a string case

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum_ = 0
-    if roman_string is None and roman_string is not str:
+    if not roman_string and roman_string is None:
         return 0
     length = len(roman_string)
     if length > 0:
