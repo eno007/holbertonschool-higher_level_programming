b449f24701979ae41d57201e36a56ef7ad1b199b
eno007 <eno.gupe@gmail.com>
2022-09-19 13:12:56 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""OP 1"""
+
+
+class Square:
+    """Square with private instance attribute"""
+    def __init__(self, size):
+        """Initialize data"""
+        self.__size = size
+        """Private instantiation of size"""
