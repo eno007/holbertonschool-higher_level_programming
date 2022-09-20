9d0fe613a6258dbd1ac82d98ee2ab9f5da020919
eno007 <eno.gupe@gmail.com>
2022-09-20 10:25:52 -0700

    Update conditions

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,9 @@
 def say_my_name(first_name, last_name=""):
     """Returns name and surname"""
     if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
+        if not first_name:
+            raise TypeError('first_name must be a string')
     if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
+        if not last_name:
+            raise TypeError('last_name must be a string')
     print("My name is {:s} {:s}".format(first_name, last_name))
