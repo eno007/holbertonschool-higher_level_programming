53acc372e480575895ea3cc024f1892ce0434737
eno007 <eno.gupe@gmail.com>
2022-09-20 06:26:46 -0700

    Task 1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Matrix_divided function"""
+
+
+def matrix_divided(matrix, div):
+    """Returns the sum of a + b"""
+
+    if len({len(list) for list in matrix}) != 1:
+        raise TypeError('Each row of the matrix must have the same size')
+    for list in matrix:
+        for el in list:
+            if type(el) is float:
+                el = int(el)
+            elif type(el) is not int:
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if type(div) is float:
+        div = int(div)
+    if type(div) is not int:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    return [[float("{:.2f}".format(el/div)) for el in list] for list in matrix]
