da34b83830113e8d1616cff1558a96fe59c8cc70
eno007 <eno.gupe@gmail.com>
2022-09-20 06:41:45 -0700

    update the function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,21 +3,32 @@
 
 
 def matrix_divided(matrix, div):
-    """Returns the sum of a + b"""
+    """Returns result of division of matrix by div"""
+    for i in matrix:
+        for j in i:
+            if type(j) is not int and type(j) is not float:
+                raise TypeError(
+                    'matrix must be a matrix (list of lists) ' +
+                    'of integers/floats')
 
-    if len({len(list) for list in matrix}) != 1:
-        raise TypeError('Each row of the matrix must have the same size')
-    for list in matrix:
-        for el in list:
-            if type(el) is float:
-                el = int(el)
-            elif type(el) is not int:
-                raise TypeError('matrix must be a matrix 
-                        (list of lists) of integers/floats')
-    if type(div) is float:
-        div = int(div)
-    if type(div) is not int:
-        raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
-    return [[float("{:.2f}".format(el/div)) for el in list] for list in matrix]
+        if type(i) is not list:
+            raise TypeError(
+                'matrix must be a matrix (list of lists) ' +
+                'of integers/floats')
+        if len(i) is not len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+
+        if type(div) is not int and type(div) is not float:
+            raise TypeError('div must be a number')
+
+        if div is 0:
+            raise ZeroDivisionError('division by zero')
+
+        matrix_div = []
+
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(round(j / div, 2))
+        matrix_div.append(row)
+    return (matrix_div)
