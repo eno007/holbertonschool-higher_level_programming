a00134bfe4ea7aef51568b2dc070a3dbeea6943e
eno007 <eno.gupe@gmail.com>
2022-09-20 03:42:10 -0700

    Task 0 and other

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Add_integer function"""
+
+
+def add_integer(a, b=98):
+    """Returns the sum of a + b"""
+    if type(a) is float:
+        a = int(a)
+    elif type(a) is not int:
+        raise TypeError('a must be an integer')
+    if type(b) is float:
+        b = int(b)
+    if type(b) is not int:
+        raise TypeError('b must be an integer')
+    return (a + b)
