db54f1d1cbffeaba3f45468ae1ee220b36b28cc7
eno007 <eno.gupe@gmail.com>
2022-09-21 03:09:48 -0700

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,13 +7,13 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
     symbols = [".", "?", ":"]
-        new_str = ""
-        for i in range(len(text)):
-            if text[i] == " " and flag == 1:
-                continue
-            flag = 0
-            new_str += text[i]
-            if text[i] in symbols:
-                new_str += "\n" + "\n"
-                flag = 1
-        print(new_str, end="")
+    new_str = ""
+    for i in range(len(text)):
+        if text[i] == " " and flag == 1:
+            continue
+        flag = 0
+        new_str += text[i]
+        if text[i] in symbols:
+            new_str += "\n" + "\n"
+            flag = 1
+    print(new_str, end="")
