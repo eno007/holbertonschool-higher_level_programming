4843f7368d2a45629b9a1c350f79909f5f03bb37
eno007 <eno.gupe@gmail.com>
2022-09-21 01:38:05 -0700

    update

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,10 @@ def say_my_name(first_name, last_name=""):
     """Returns name and surname"""
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
+    if first_name is None:
+        first_name = ""
     if type(last_name) is not str:
         raise TypeError('last_name must be a string')
+    if last_name is None:
+        last_name = ""
     print("My name is {:s} {:s}".format(first_name, last_name))
