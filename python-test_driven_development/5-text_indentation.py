c7cd1a406aa245f2db8c4bcc04f20b7ad8443bac
eno007 <eno.gupe@gmail.com>
2022-09-21 02:46:29 -0700

    update task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Text_indentation function"""
+
+
+def text_indentation(text):    
+    """Prints indentation text"""
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    string = ''
+    for el in text:
+        if el in '.?:':
+            string += el
+            print(string.strip(' '))
+            print()
+            string = ''
+        else:
+            string += el
+    print(string.strip(' '), end='')
