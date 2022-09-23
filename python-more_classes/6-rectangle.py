a57104e78ec59d78f1af8ea02e3b98d7b3150243
eno007 <eno.gupe@gmail.com>
2022-09-23 01:22:23 -0700

    Pep8

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,10 +5,8 @@
 class Rectangle:
     """Rectangle class"""
 
-
     number_of_instances = 0
     """Counts number of instances"""
-
 
     def __init__(self, width=0, height=0):
         """Initialize data"""
