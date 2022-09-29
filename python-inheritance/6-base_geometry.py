37884a6aad5e071128686be96def3fda8fa90154
eno007 <eno.gupe@gmail.com>
2022-09-29 04:06:13 -0700

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 5"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """Public instance method"""
+        raise Exception:
+            area() is not implemented
