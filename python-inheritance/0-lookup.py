e4f9336c8c4ee93f6ad4a677f5a33c2a64409bb2
eno007 <eno.gupe@gmail.com>
2022-09-29 02:01:38 -0700

    Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Function that returns the list of available 
+"""Function that returns the list of available
 attributes and methods of an object"""
 
 
 def lookup(obj):
-    """Function that returns the list of available 
+    """Function that returns the list of available
     attributes and methods of an object"""
-
-
     return dir(obj)
