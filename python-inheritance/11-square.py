be23f6c3408194d4d70603e067eddebfb226e96e
eno007 <eno.gupe@gmail.com>
2022-09-29 05:15:12 -0700

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from 'Rectangle'"""
+    def __init__(self, size):
+        """Instantiation with private size attribute"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Function to return area of square"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """Method to print Rectangle"""
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
