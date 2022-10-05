0bd98a79f43bd06d0ebbe9d3b2bb0ea68812010e
eno007 <eno.gupe@gmail.com>
2022-10-05 04:23:42 -0700

    Task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,11 @@ class Base():
                 list_dict += [i.to_dictionary()]
         with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
             f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns list of JSON string representation"""
+        if json_string is None:
+            return ([])
+        else:
+            return (json.loads(json_string))
