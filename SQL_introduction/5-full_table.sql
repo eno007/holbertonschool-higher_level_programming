37cc5d538a1dea5b315115a68fa51aab0e81ad1b
eno007 <eno.gupe@gmail.com>
2022-11-04 03:07:47 -0700

    Task 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Command that prints the full description of the table required from the database in mysql server, database name will be passed as an argument of mysql command
+SHOW CREATE TABLE first_table;
