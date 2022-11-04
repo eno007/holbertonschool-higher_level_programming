80ceabbf3e9ea9bf3a662aacd91f0af872d44315
eno007 <eno.gupe@gmail.com>
2022-11-04 03:53:31 -0700

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- Script that creates a table as it is given in the required database with multiple rows, it wont fail if the table exists name will be passes as an argument in the command
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT,
 	name VARCHAR(256),
 	score INT
 );
 
 -- Inserting rows as required
 INSERT INTO second_table
-VALUES ("John", 10),
-("Alex", 3)
-("Bob", 14)
-("George", 8);
+VALUES (1, "John", 10),
+(2, "Alex", 3)
+(3, "Bob", 14)
+(4, "George", 8);
