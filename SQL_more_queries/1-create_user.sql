2d0e2b13632dcb932ee6e581f2bd46142289bf4b
eno007 <eno.gupe@gmail.com>
2022-11-09 01:18:36 -0800

    Task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Script that creates user_0d_1 in MYSQL with all privileges  password should be set to user_0d_1_pwdand If the user user_0d_1 already exists, your script should not fail
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
