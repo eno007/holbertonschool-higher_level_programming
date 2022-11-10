ad9b60d1f6ee26a46fb9d190cd4cc94d9de504dd
eno007 <eno.gupe@gmail.com>
2022-11-10 04:14:43 -0800

    Task 4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,5 @@
---script that creates the table id_not_null on your MySQL server, requested rows, database name will be passed as an argument of the mysql command, If the table id_not_null already exists, your script should not fail
+--script that creates the table `id_not_null` on your MySQL server, 
+--requested rows, database name will be passed as an argument of the mysql command, If the table id_not_null already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS id_not_null (
 	id int DEFAULT 1,
 	name VARCHAR(256)
